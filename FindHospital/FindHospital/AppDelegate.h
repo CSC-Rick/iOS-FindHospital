7a0f3b9f6bae77a3d71fbc6ca4556e1058c3a20e
Rick <rxu4@csc.com>
2012-01-19 12:00:22 +0800

    Test

---
+++ FindHospital/FindHospital/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FindHospital
+//
+//  Created by rickx on 1/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
